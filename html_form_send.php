3be85a9181df96e7b17422f00a85a3ef1e6aa77d
Virginia Peters <vpetersdesign@gmail.com>
2015-05-24 21:40:26 -0400

    contact form, email changed

--- html_form_send.php
+++ html_form_send.php
@@ -2,9 +2,14 @@
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n message: $message";
-$recipient = "support@batonhandoff.com";
+$recipient = "vpetersdesign@gmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!";
+echo $name;
+echo $email;
+echo $message;
+echo $formcontent;
+
 ?>
